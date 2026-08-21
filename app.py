d2dfa07074eee72270dfdc8e2d9e861c3d0fa644
Shalien14 <shaliennirmalan14@gmail.com>
2026-08-21 19:40:42 +0530

    showing entered text in website

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ app=Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
 
+    note=""
+
     if request.method == "POST":
         note=request.form["note"]
         print(note)
-    return render_template("index.html")
+    return render_template("index.html",note=note)
 
 if __name__=="__main__":
     app.run(debug=True)
