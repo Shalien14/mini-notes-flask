1db2820540efa5cc1b564a47422f3a4f92f61684
Shalien14 <shaliennirmalan14@gmail.com>
2026-09-02 10:29:37 +0530

    adding delete ffeature for notes

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app=Flask(__name__)
 
-notes=[]
 
+
+connection = sqlite3.connect("my_notes.db")
+connection.execute("""
+CREATE TABLE IF NOT EXISTS notes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT NOT NULL
+)
+
+""")
+connection.commit()
+connection.close()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
 
     connection = sqlite3.connect("my_notes.db")
-
-    connection.execute("""
-    CREATE TABLE IF NOT EXISTS notes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        content TEXT NOT NULL
-    )
-    
-    """)
-    connection.commit()
 
     if request.method == "POST":
         note=request.form["note"]
@@ -28,11 +29,24 @@ def home():
             (note,)
         )
         connection.commit()
-        rows = connection.execute("SELECT * FROM notes").fetchall()
-
-        connection.close()
+        return redirect("/")
+    rows = connection.execute("SELECT * FROM notes").fetchall()
+    connection.close()
 
     return render_template("index.html",notes=rows)
-    
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    note_id=request.form["id"]
+
+    connection = sqlite3.connect("my_notes.db")
+    connection.execute(
+        "DELETE FROM notes WHERE id= ?",
+        (note_id,)
+    )
+    connection.commit()
+    connection.close()
+
+    return redirect("/")
 if __name__=="__main__":
     app.run(debug=True)
